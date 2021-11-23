b174a8c152af21c630baf78c87b7be0efd57b55a
Ronak Patel <ronak0624@gmail.com>
2021-11-23 12:38:41 -0800

    redirect to specified video

--- lambda/share.js
+++ lambda/share.js
@@ -3,32 +3,45 @@ const path = require('path')
 
 exports.handler = async function (event, context) {
     var { headers, path } = event;
-    let agent = headers["user-agent"];
+    var agent = headers["user-agent"];
     var title;
-    if(path) {
-        title = path.split("/.netlify/functions/share")[1].split("/").join(" ").trim()
+    var query;
+
+    if(agent.includes("+http")) {
+        var title;
+        if(path) {
+            query = path.split("/.netlify/functions/share")[1].split("/")
+            title = query.join(" ").trim()
+        }
+    
+        console.log(title)
+        console.log(agent)
+    
+        let message =
+            `
+                <!DOCTYPE html>
+                <html lang="en">
+                    <head>
+                        <title>${title}</title>
+                        <meta name="title" content="${title}">
+                        <meta name="description" content="Dynamically created">
+    
+                        <meta property="og:type" content="website">
+                        <meta property="og:title" content="${title}">
+                        <meta property="og:description" content="Dynamically created">
+                    </head>
+                </html>
+            `
+        return {
+            statusCode: 200,
+            body: message
+        };
+    } else {
+        return {
+            statusCode: 302,
+            headers: {
+                Location: `https://projectspeakeasy.com/app/journal${query.join("/")}`
+            }
+        }
     }
-
-    console.log(title)
-    console.log(agent)
-
-    let message =
-        `
-            <!DOCTYPE html>
-            <html lang="en">
-                <head>
-                    <title>${title}</title>
-                    <meta name="title" content="${title}">
-                    <meta name="description" content="Dynamically created">
-
-                    <meta property="og:type" content="website">
-                    <meta property="og:title" content="${title}">
-                    <meta property="og:description" content="Dynamically created">
-                </head>
-            </html>
-        `
-    return {
-        statusCode: 200,
-        body: message
-    };
 }
