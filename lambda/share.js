b54381ff2f7c79857f882ae8b03256a839e46988
Ronak Patel <ronak0624@gmail.com>
2021-11-23 01:14:24 -0800

    more lgging

--- lambda/share.js
+++ lambda/share.js
@@ -2,13 +2,14 @@ const fs = require('fs')
 const path = require('path')
 
 exports.handler = async function (event, context) {
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Logo.KrispyKreme.svg/1024px-Logo.KrispyKreme.svg.png'
     var { headers } = event;
     let agent = headers["user-agent"];
-    let query = headers["path"].split("/.netlify/functions/share")[1]
+    let query = headers["path"]
     let title = query.replace("/", " ")
 
     console.log(agent)
+    console.log(headers)
+    console.log(event)
 
     let message =
         `
