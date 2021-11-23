e9ab2db0ba8d3eb5709a2156a07e0f256d37ea4a
Ronak Patel <ronak0624@gmail.com>
2021-11-23 08:59:24 -0800

    removed stock image

--- lambda/share.js
+++ lambda/share.js
@@ -17,15 +17,13 @@ exports.handler = async function (event, context) {
             <!DOCTYPE html>
             <html lang="en">
                 <head>
-                    <title><${title}</title>
+                    <title>${title}</title>
                     <meta name="title" content="${title}">
                     <meta name="description" content="Dynamically created">
 
                     <meta property="og:type" content="website">
-                    <meta property="og:url" content='https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Logo.KrispyKreme.svg/1024px-Logo.KrispyKreme.svg.png'>
                     <meta property="og:title" content="${title}">
                     <meta property="og:description" content="Dynamically created">
-                    <meta property="og:image" content="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Logo.KrispyKreme.svg/1024px-Logo.KrispyKreme.svg.png">
                 </head>
             </html>
         `
