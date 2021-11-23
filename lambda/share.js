84b592d677bdfa986c4b2d1b37ee20f6d687773f
Ronak Patel <ronak0624@gmail.com>
2021-11-22 22:09:10 -0800

    logging env stuff

--- lambda/share.js
+++ lambda/share.js
@@ -2,8 +2,12 @@ const fs = require('fs')
 const path = require('path')
 
 exports.handler = async function (event, context) {
+    console.log(event)
+    console.log(context)
+    console.log(path.join(__dirname))
+    
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: fs, status: "AYO"})
+        body: JSON.stringify({ status: "AYO"})
     };
 }
