0051220ce5f29ff4f9458dcf376bf8eca1488e15
Ronak Patel <ronak0624@gmail.com>
2021-11-23 14:03:00 -0800

    title, removed preview image

--- lambda/share.js
+++ lambda/share.js
@@ -10,7 +10,7 @@ exports.handler = async function (event, context) {
         let speakeasyURL = `https://projectspeakeasy.com/app/journal${query.join("/")}`
 
         if (agent.includes("+http")) {
-            var title = query.join(" ").trim()
+            var title = `VIDEO ID: ${query[0]}`
 
             console.log(title)
             console.log(agent)
@@ -30,14 +30,11 @@ exports.handler = async function (event, context) {
                         <meta property="og:url" content="${speakeasyURL}">
                         <meta property="og:title" content="${title}">
                         <meta property="og:description" content="${description}">
-                        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
 
                         <!-- Twitter -->
-                        <meta property="twitter:card" content="summary_large_image">
                         <meta property="twitter:url" content="${speakeasyURL}">
                         <meta property="twitter:title" content="${title}">
                         <meta property="twitter:description" content="${description}">
-                        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
                     </head>
                 </html>
             `
